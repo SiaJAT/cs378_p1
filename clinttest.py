82a79bb73e38d190485eccab8b20c4419b95a2a8
Clint Ascencio <clintascencio@hotmail.com>
2014-09-21 14:25:48 -0500

    Added new photos. Tests still fail.

--- clinttest.py
+++ clinttest.py
@@ -2,13 +2,13 @@ import cv2
 import pano_stitcher
 import numpy
 
-# left = cv2.imread("my_panos//pano1.jpg",-1)
-# right = cv2.imread("my_panos//pano2.jpg")
-# middle = cv2.imread("my_panos//pano3.jpg",-1)
+left = cv2.imread("my_panos//pano4.jpg",-1)
+right = cv2.imread("my_panos//pano6.jpg")
+middle = cv2.imread("my_panos//pano5.jpg",-1)
 
-left = cv2.imread("test_data//books_1.png", -1)
-middle = cv2.imread("test_data//books_2.png")
-right = cv2.imread("test_data//books_3.png", -1)
+# left = cv2.imread("test_data//books_1.png", -1)
+# middle = cv2.imread("test_data//books_2.png")
+# right = cv2.imread("test_data//books_3.png", -1)
 
 leftHomo = pano_stitcher.homography(middle, left)
 warpedLeft, leftOrigin = pano_stitcher.warp_image(left, leftHomo)
