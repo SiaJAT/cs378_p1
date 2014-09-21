29e76488557d6cc8ccac1bcdc411c36a1343dcf9
Tim Sherlock <the.tim.sherlock@gmail.com>
2014-09-21 12:41:12 -0500

    somewhat working

--- clinttest.py
+++ clinttest.py
@@ -13,21 +13,28 @@ pano1 = cv2.imread("test_data//books_1.png", -1)
 pano2 = cv2.imread("test_data//books_2.png") 
  
 pano3 = cv2.imread("test_data//books_3.png", -1)
+pano3w = cv2.imread("test_data//books_3_warped.png", -1)
+
 
 pano12Homography = pano_stitcher.homography(pano2, pano1) 
 warpedPano12, tup12 = pano_stitcher.warp_image(pano1, pano12Homography) 
+# cv2.imshow("warped 1 2", warpedPano12) 
+# cv2.waitKey() 
+
+pano23Homography = pano_stitcher.homography(pano2, pano3) 
+warpedPano23, tup23 = pano_stitcher.warp_image(pano3, pano23Homography) 
+print warpedPano23.shape
+# cv2.imshow("warped 3 2", warpedPano23) 
+# cv2.imshow("regular", pano3w)
+# cv2.waitKey() 
+
 print tup12 
-cv2.imshow("warped 1 2", warpedPano12) 
-cv2.waitKey() 
+print tup23 
+tup23fix = (round(tup23[0]), round(-1*tup23[1]))
+tup12 = (round(tup12[0]), round(tup12[1]))
 
-# pano23Homography = pano_stitcher.homography(pano2, pano3) 
-# warpedPano23, tup23 = pano_stitcher.warp_image(pano2, pano23Homography) 
-
-
-# print tup23 
-# print tup12 
-images = (warpedPano12, pano2) 
-# pano2Origins = (0, 0) 
-origins = (tup12, (0, 0)) 
+pano2 = cv2.cvtColor(pano2, cv2.COLOR_BGR2BGRA) 
+images = (warpedPano23, warpedPano12, pano2) 
+origins = (tup23fix,tup12,(0, 0))
 pano = pano_stitcher.create_mosaic(images, origins) 
 cv2.imwrite("myPanoTest.png", pano) # print tup3
--- pano_stitcher.py
+++ pano_stitcher.py
@@ -10,6 +10,7 @@ TODO: Implement!
 import cv2
 import numpy as np
 from matplotlib import pyplot as plt
+import math
 
 
 def homography(image_a, image_b, bff_match=False):
@@ -25,7 +26,7 @@ def homography(image_a, image_b, bff_match=False):
 
     MIN_MATCH_COUNT = 10
 
-    sift = cv2.SIFT()
+    sift = cv2.SIFT(edgeThreshold=10, sigma = 1.25, contrastThreshold=0.08)
 
     kp_a, des_a = sift.detectAndCompute(image_a, None)
     kp_b, des_b = sift.detectAndCompute(image_b, None)
@@ -39,7 +40,6 @@ def homography(image_a, image_b, bff_match=False):
         if m.distance < .9 * n.distance:
             good.append(m)
 
-    print len(good)
     if len(good) > MIN_MATCH_COUNT:
         src_pts = np.float32([kp_a[m.queryIdx].pt for m in good])\
             .reshape(-1, 1, 2)
@@ -49,7 +49,7 @@ def homography(image_a, image_b, bff_match=False):
     # cv2.waitKey()
     # cv2.destroyAllWindows()
 
-    M, mask = cv2.findHomography(dst_pts, src_pts, cv2.RANSAC, 3)
+    M, mask = cv2.findHomography(dst_pts, src_pts, cv2.RANSAC, 5)
     return M
 
 
@@ -72,12 +72,9 @@ def warp_image(image, homography):
         offset translation component of the homography.
     """
 
-    print homography
+    # print homography
     image = cv2.cvtColor(image, cv2.COLOR_BGR2BGRA) 
     h, w, z = image.shape 
-
-    # homo_inv = np.linalg.inv(homography)
-
 
     upperLeft = np.array([[0], [0], [1]]) 
     upperRight = np.array([[w], [0], [1]])
@@ -89,16 +86,10 @@ def warp_image(image, homography):
     tbl = np.dot(homography, bottomLeft)
     tbr = np.dot(homography, bottomRight)
 
-    pts1 = np.float32([[0,0], [w, 0], [0, h], [w, h]])
-    pts2 = np.float32([[tul[0][0], tul[1][0]],[tur[0][0], tur[1][0]],[tbl[0][0], tbl[1][0]],[tbr[0][0], tbr[1][0]]])
-
-    M = cv2.getPerspectiveTransform(pts1,pts2)
-    # print transformUpperLeft, transformUpperRight, transformBottomRight, transformBottomLeft
-
     p = np.array([[0, w, w, 0], [0, 0, h, h], [1, 1, 1, 1]])
 
     p_prime = np.dot(homography, p)
-
+    # print "p prime"
     # print p_prime
 
     yrow = p_prime[1]*(1/p_prime[2])
@@ -108,100 +99,68 @@ def warp_image(image, homography):
     ymax = max(yrow)
     xmax = max(xrow)
 
-    if ymin < 0:
-        homography[1,2] = homography[1,2]-ymin
-    if xmin < 0:
-        homography[0,2] = homography[0,2]-xmin
+    # print (xmin, xmax)
 
-    print homography
+    new_mat = np.array([[1,0,-1*xmin],[0,1,-1*ymin],[0,0,1]])
+
+    homography = np.dot(new_mat, homography)
+    # print homography
     asList = list(tul) 
     tup = (asList[0][0], asList[1][0]) 
-    
-    # x_scale = int(round(homo_inv[0, 0])) 
-    # y_scale = int(round(homo_inv[1, 1])) 
-    # x_translate = int(round(homo_inv[0,2]))
-    # y_translate = int(round(homo_inv[1,2]))
 
-    # height = (y_scale * h) 
-    # width = (x_scale * w)
-    # print height
-    # print width
+    height = int(round(ymax-ymin))
+    width = int(round(xmax-xmin))
+    
    
-    stitch = cv2.warpPerspective(src = image, M = homography, dsize = (int(round(xmax-xmin)), int(round(ymax-ymin))))
-    print stitch.shape
+    stitch = cv2.warpPerspective(src = image, M = homography, dsize = (width, height))
+    # print stitch.shape
     return stitch, tup
 
-
-
 def create_mosaic(images, origins):
-    """Combine multiple images into a mosaic.
-
-    Arguments:
-      images: a list of 4-channel images to combine in the mosaic.
-      origins: a list of the locations upper-left corner of each image in
-               a common frame, e.g. the frame of a central image.
-
-    Returns: a new 4-channel mosaic combining all of the input images. pixels
-             in the mosaic not covered by any input image should have their
-             alpha channel set to zero.
-    """
-    print origins
     mapped = map(lambda x, y: (x, y), origins, images)
+    dist_sorted = sorted(mapped, key=lambda x: math.sqrt(x[0][0]**2 + x[0][1]**2), reverse= True)
+    x_sorted = sorted(mapped, key=lambda x: x[0][0])
+    y_sorted = sorted(mapped, key=lambda x: x[0][1])
 
-    mapped_sorted = sorted(mapped, key=lambda x: x[0])
+    width = x_sorted[-1][1].shape[1]
+    for i in range(0, len(x_sorted)):
+        width += np.abs(x_sorted[i][0][0])
 
-    width = mapped_sorted[-1][1].shape[1]
-    for x in range(0, len(mapped_sorted)):
-        width += np.abs(mapped_sorted[x][0][0])
+    height = y_sorted[0][1].shape[0] + y_sorted[0][0][1]
+    print "start height " + str(height)
+    for image in y_sorted:
+        height = max(height, -1*y_sorted[0][0][1]+image[1].shape[0]+image[0][1])
 
-    origins_diffs = []
-    i = 0
-    j = len(mapped_sorted) - 1
-    mid = len(mapped_sorted) / 2
-    while i <= mid and j > mid:
-        origins_diffs.append(
-            np.abs(mapped_sorted[i][0][1]) -
-            np.abs(mapped_sorted[j][0][1]))
-        i = i + 1
-        j = j - 1
-
-    max_origin_diff = max(origins_diffs)
-
-    height = max([img.shape[0] for img in images]) + max_origin_diff
-    print str(height) + " the height"
+    print "width, height " + str((width, height))
 
     stitch = np.zeros((height, width, 4), np.uint8)
 
-    left = mapped_sorted[0]
+    if x_sorted[0][0][0] > 0:
+        cent_x = 0 # leftmost image is central image
+    else :
+        cent_x = abs(x_sorted[0][0][0])
 
-    # Get Leftmost height and width
-    leftX2 = left[1].shape[0]
-    leftY2 = left[1].shape[1]
+    if y_sorted[0][0][1] > 0:
+        cent_y = 0 # topmost image is central image
+    else :
+        cent_y = abs(y_sorted[0][0][1])
 
-    # stitch the leftmost image
-    stitch[:leftX2, :leftY2, :4] = left[1]
+    central_image = (cent_x, cent_y)
+    print central_image
 
-    # get the rightmost image
-    right = mapped_sorted[-1]
+    for image in dist_sorted:
+        start_y = image[0][1] + central_image[1]
+        start_x = image[0][0] + central_image[0]
+        end_y = start_y + image[1].shape[0]
+        end_x = start_x + image[1].shape[1]
+        print image[1].shape
+        print "start x start y" + str((start_x, start_y))
+        print "end_x end_y" + str((end_x, end_y))
+        
 
-    # rightmost image
-    rightX2 = np.abs(left[0][1]) - np.abs(right[0][1])
-    rightY2 = np.abs(left[0][0]) + np.abs(right[0][0])
+        stitch[start_y:end_y, start_x:end_x, :4] = image[1]
 
-    # append the rightmost image to the stitch
-    stitch[rightX2:, rightY2:, :4] = right[1]
-
-    # append middle images one by one
-    for x in range(1, len(mapped_sorted) - 1):
-        meetX1 = np.abs(mapped_sorted[x - 1][0][1])
-        meetY1 = np.abs(mapped_sorted[x - 1][0][0])
-
-        spanX2 = meetX1 + mapped_sorted[x][1].shape[0]
-        spanY2 = meetY1 + mapped_sorted[x][1].shape[1]
-
-        stitch[meetX1:spanX2, meetY1:spanY2, :4] = mapped_sorted[x][1]
-    # cv2.imshow('img', stitch)
-    # cv2.waitKey(0)
+    cv2.imshow('img', stitch)
+    cv2.waitKey(0)
 
     return stitch
-    # pass
